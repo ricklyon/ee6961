5ea56d2c0f790b36d8f8c926bf3fe3a5c48a7035
Grant Brown <grantb91@gmail.com>
2024-04-18 21:55:41 -0600

    updates, bug on hls

--- project_part3/utils.py
+++ project_part3/utils.py
@@ -4,3 +4,12 @@ import numpy as np
 def dftmtx(N):
     dftmtx = np.fft.fft(np.eye(N))
     return dftmtx
+
+def non_square_dftmtx(data, L, K):
+    V = np.empty([data.shape[0], L], dtype=np.complex64)
+    for i in range(0, data.shape[0]):
+        for j in range(0, L):
+            test = data[i]
+            V[i][j] = np.exp(-1j*2*np.pi*(data[i]*(j/K)))
+    return V
+        
